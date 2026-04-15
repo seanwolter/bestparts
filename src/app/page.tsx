66a53bb1e33dc22ba842203265cc181aa5175305
mgriffioen <mgriffioen@gmail.com>
2026-04-15 10:10:07 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default async function Home({
     <div>
       <div className="mb-10">
         <h1 className="text-3xl font-black text-white mb-2">
-          The best parts of movies (not miniseries)
+          The best parts of movies
         </h1>
       </div>
       <HomeSortControls sort={sort} />
